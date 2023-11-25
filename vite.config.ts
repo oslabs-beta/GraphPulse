6477b7d881f621720a1469b1543eb8b83296e6af
Jerel Cruz <125933755+jaycruz2905@users.noreply.github.com>
2023-11-25 14:37:35 -0800

    Update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  },
+  preview: {
+    port: 8080,
+  }
 })
