654ac49908bbbf9fbc8b9cb5c861cda78046ffff
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-12-07 18:41:13 -0800

    Add url input bar and send button

--- src/client/components/RightContainer/RContComponents/EndPointBar.jsx
+++ src/client/components/RightContainer/RContComponents/EndPointBar.jsx
@@ -3,6 +3,12 @@ import React from "react";
 
 function EndPointBar() {
 
+  return (
+    <div id="endpoint-bar">
+      <input id="endpoint-input" type="text" placeholder="Enter URL or endpoint"/>
+      <button id="endpoint-send-btn">Send</button>
+    </div>
+  )
 
 }
 
