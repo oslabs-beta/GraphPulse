8d76bcf8a7087e3d63af0665f72e391da25e5c93
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-12-07 18:42:16 -0800

    Import and display EndPointBar, GraphContainer, and QLogsContainer components

--- src/client/components/RightContainer/RightContainer.jsx
+++ src/client/components/RightContainer/RightContainer.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
+import EndPointBar from "./RContComponents/EndPointBar";
+import GraphContainer from "./RContComponents/GraphContainer";
+import QLogsContainer from "./RContComponents/QLogsContainer";
+
 
 function RightContainer() {
-
+  return (
+    <div id="right-container">
+      <EndPointBar />
+      <GraphContainer />
+      <QLogsContainer />
+    </div>
+  )
 
 }
 
