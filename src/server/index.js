20aaf915158038a916a296c47e7d057f2c57e32b
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2023-12-09 15:52:15 -0500

    Serve assets folder

--- src/server/index.js
+++ src/server/index.js
@@ -14,6 +14,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use('/assets', express.static(path.resolve(__dirname, '../client/assets')));
 app.use('/', express.static(path.resolve(__dirname, '../../dist')));
 
 const server = new ApolloServer({
