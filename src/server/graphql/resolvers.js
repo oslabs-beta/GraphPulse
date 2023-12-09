166ebcc42029b83374160a959254196232e08f9b
Binette Maggio <bibaswe@gmail.com>
2023-12-09 10:25:44 -0600

    add mutations

--- src/server/graphql/resolvers.js
+++ src/server/graphql/resolvers.js
@@ -11,6 +11,35 @@ const resolvers = {
       return user;
     },
   },
+
+  Mutation: {
+    createUser: (parent, args) => {
+      const user = args.input;
+      const previousId = users[users.length - 1].id;
+      user.id = previousId + 1;
+      users.push(user);
+      return user;
+    },
+
+    updateUser: (parent, args) => {
+      const { id, newUsername } = args.input;
+      let userUpdated;
+      users.forEach((user) => {
+        if (user.id === Number(id)) {
+          user.username = newUsername;
+          userUpdated = user;
+        }
+      });
+
+      return userUpdated;
+    },
+
+    deleteUser: (parent, args) => {
+      const id = args.id;
+      users.remove((user) => user.id === Number(id));
+      return null;
+    },
+  },
 };
 
 module.exports = resolvers;
