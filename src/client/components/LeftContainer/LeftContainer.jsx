7e64370d712de1675b7daff2d68e7d111b10239f
Jerel Cruz <125933755+jaycruz2905@users.noreply.github.com>
2024-01-04 19:20:26 -0800

    Resolve merge conflict

---
+++ src/client/components/LeftContainer/LeftContainer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Navbar from "./LContComponents/Navbar"
+import EndPointBar from "./LContComponents/EndPointBar";
+import QLogInput from './LContComponents/QLogInput';
+import QLogInput from "./LContComponents/QLogInput";
+
+
+function LeftContainer() {
+    return (
+        <div id="left-container">
+            <div id="graph-pulse-header">
+                <img id="graph-pulse-icon" src="../../assets/grahpqllogo.png" alt="GraphPulse Logo"/>
+                <p id="projectTitle">Graph Pulse</p>
+            </div>
+            <Navbar />
+            <QLogInput />
+            <QLogOutput />
+            <EndPointBar />
+        </div>
+    );
+}
+
+export default LeftContainer;
