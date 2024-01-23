1202a3f65ebda5a060bc79a1ad1293e36eee4d12
Binette Maggio <bibaswe@gmail.com>
2024-01-23 02:02:20 -0600

    Integrate QLogInput/Editor component

--- src/client/components/LeftContainer/LContComponents/QLogInput.jsx
+++ src/client/components/LeftContainer/LContComponents/QLogInput.jsx
@@ -1,18 +1,85 @@
-import React, { useState, useEffect } from 'react';
-// import { GraphiQL } from 'graphiql';
-// import { createGraphiQLFetcher } from '@graphiql/toolkit';
+import React, { useState } from 'react';
+import { request } from 'graphql-request';
+import MonacoEditor from 'react-monaco-editor';
+import Split from 'react-split';
 
-const QLogInput = ({}) => {
-  const [qInput, setQInput] = useState('');
+// Default operations for the Monaco editor
+const defaultOperation = `
+# Enter a query #
+
+query {
+
+}
+`;
+
+function QLogInput() {
+  // States for query and results
+  const [query, setQuery] = useState(defaultOperation);
+  const [results, setResults] = useState('');
+
+  // Event handler for query input changes
+  const handleQueryChange = (newQuery) => {
+    setQuery(newQuery);
+  };
+
+  // Event handler for running the query
+  const handleRunQuery = () => {
+    // Test for mock data
+
+    const resultString = [
+      'user1user1@example.com1',
+      'user2user2@example.com1',
+      'user3user3@example.com1',
+      'user4user4@example.com1',
+      'user5user5@example.com1',
+    ].join('\n');
+
+    setResults(resultString);
+  };
 
   return (
-    <div className="inputArea">
-      <h1>QLOG INPUT</h1>
-      <form>
-        <textarea value={qInput}></textarea>
-      </form>
+    <div className="monaco-container">
+      <Split
+        sizes={[50, 50]}
+        minSize={5}
+        expandToMin={false}
+        gutterSize={10}
+        gutterAlign="center"
+        dragInterval={1}
+        direction="horizontal"
+        cursor="col-resize"
+        className="query-results-split"
+      >
+        <section className="editor-container query-editor">
+          <MonacoEditor
+            language="graphql"
+            theme="vs-dark"
+            value={query}
+            onChange={handleQueryChange}
+            height="300"
+            width="200%"
+          />
+          <button
+            id="input-run-btn"
+            onClick={handleRunQuery}
+            className="run-query-button"
+          >
+            Run Query
+          </button>
+        </section>
+        <section className="editor-container results-editor">
+          <MonacoEditor
+            language="plaintext"
+            theme="vs-dark"
+            value={results}
+            height="300"
+            width="200%"
+            readOnly
+          />
+        </section>
+      </Split>
     </div>
   );
-};
+}
 
 export default QLogInput;
