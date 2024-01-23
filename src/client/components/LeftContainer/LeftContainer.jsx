da812d294c2e61e91d023c62ceb2a0e7bcdb1e21
Binette Maggio <bibaswe@gmail.com>
2024-01-23 02:08:03 -0600

    update LeftContainer component

--- src/client/components/LeftContainer/LeftContainer.jsx
+++ src/client/components/LeftContainer/LeftContainer.jsx
@@ -1,24 +1,26 @@
-import React from "react";
-
-import Navbar from "./LContComponents/Navbar"
-import EndPointBar from "./LContComponents/EndPointBar";
+import React from 'react';
+import Navbar from './LContComponents/Navbar';
+import EndPointBar from './LContComponents/EndPointBar';
 import QLogInput from './LContComponents/QLogInput';
-import QLogOutput from "./LContComponents/QLogOutput";
+import QLogOutput from './LContComponents/QLogOutput';
 
-
-function LeftContainer() {
-    return (
-        <div id="left-container">
-            <div id="graph-pulse-header">
-                <img id="graph-pulse-icon" src="../../assets/grahpqllogo.png" alt="GraphPulse Logo"/>
-                <p id="project-title">GraphPulse</p>
-            </div>
-            <Navbar />
-            <QLogInput />
-            <QLogOutput />
-            <EndPointBar />
-        </div>
-    );
+function LeftContainer({ onQueryChange, endpoint, setEndpoint, query }) {
+  return (
+    <div id="left-container">
+      <div id="graph-p-header">
+        <img
+          id="graph-pulse-icon"
+          src="../../assets/grahpqllogo.png"
+          alt="GraphPulse Logo"
+        />
+        <p id="project-title">GraphPulse</p>
+      </div>
+      <Navbar />
+      <EndPointBar endpoint={endpoint} setEndpoint={setEndpoint} />
+      <QLogInput onQueryChange={onQueryChange} />
+      <QLogOutput endpoint={endpoint} query={query} />
+    </div>
+  );
 }
 
 export default LeftContainer;
