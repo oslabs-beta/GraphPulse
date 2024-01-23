2e87250c4aca4af5749253b924aef57cd7903b7e
Binette Maggio <bibaswe@gmail.com>
2024-01-23 01:39:34 -0600

    Implement EndPointBar component

--- src/client/components/LeftContainer/LContComponents/EndPointBar.jsx
+++ src/client/components/LeftContainer/LContComponents/EndPointBar.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from 'react';
+import { request } from 'graphql-request';
 
+const DEFAULT_ENDPOINT = 'https://api.mocki.io/v2/c4d7a195/graphql';
 
-function EndPointBar() {
+function EndPointBar({ setEndpoint }) {
+  const [endPointValue, setEndPointValue] = useState(DEFAULT_ENDPOINT);
+
+  // handler for the "send" button click
+  const handleEndpointSubmit = () => {
+    setEndpoint(endPointValue);
+    // console.log(endPointValue);
+    // console.log(typeof endPointValue);
+  };
 
   return (
     <div id="endpoint-container">
-      <input id="endpoint-input" type="text" placeholder="Enter URL or endpoint"/>
-      <button id="endpoint-send-btn">Send</button>
-    </div>
-  )
+      <input
+        id="endpoint-input"
+        type="text"
+        placeholder="Enter URL or endpoint 🗝"
+        value={endPointValue}
+        onChange={(e) => setEndpoint(e.target.value.trim())}
+      />
 
+      <button id="endpoint-send-btn" onClick={handleEndpointSubmit}>
+        Send
+      </button>
+    </div>
+  );
 }
 
 export default EndPointBar;
