1e64e2378e96780e8cc291d1e292121768146c88
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-01-23 18:01:31 -0800

    Create custom node for reactflow visualization

---
+++ src/client/components/RightContainer/components/QFlowNode.jsx
@@ -0,0 +1,25 @@
+import { Handle, Position } from 'reactflow';
+
+export default function QFlowNode({ data, isConnectable }) {
+  return (
+    <div>
+      <Handle
+        type='target'
+        position={Position.Top}
+        isConnectable={isConnectable}
+      />
+      <Handle 
+        type='source'
+        position={Position.Bottom}
+        id='a'
+        isConnectable={isConnectable}
+      />
+      <Handle 
+        type='source'
+        position={Position.Bottom}
+        id='b'
+        isConnectable={isConnectable}
+      />
+    </div>
+  )
+}
