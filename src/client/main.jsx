84f4f844ea9e9f8c5cbc9100de055b8105b76a9d
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-01-24 22:35:15 -0500

    Establish uri variable and update import statements

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
-});
-
-
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import styles from "./styles/RightContainer.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-    <App />
-    </ApolloProvider>
-  </React.StrictMode>,
-)
+const MyComponent = () => {
+  const [uri, setUri] = useState('http://localhost:4000/graphql');
+
+  const client = new ApolloClient({
+    uri: uri,
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+        <App uri={uri} setUri={setUri}/>
+      </ApolloProvider>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<MyComponent />);
+
+
+
