1a32c7e9053fee17d17998a591e8d044eeb37c11
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-01-24 19:22:43 -0800

    Populate custom QFlowNode component

--- src/client/components/RightContainer/components/QFlowNode.jsx
+++ src/client/components/RightContainer/components/QFlowNode.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Handle, Position } from 'reactflow';
 
 export default function QFlowNode({ data, isConnectable }) {
   return (
-    <div>
+    <div className='qflow-node'>
       <Handle
         type='target'
-        position={Position.Top}
+        position={Position.Left}
         isConnectable={isConnectable}
       />
+      <div>
+        <label htmlFor="label">{data.label}</label>
+      </div>
       <Handle 
         type='source'
-        position={Position.Bottom}
+        position={Position.Right}
         id='a'
         isConnectable={isConnectable}
+        style={{ top: 10 }}
       />
       <Handle 
         type='source'
-        position={Position.Bottom}
+        position={Position.Right}
         id='b'
         isConnectable={isConnectable}
+        style={{ top: 50 }}
       />
     </div>
   )
