41d3c35017e6161ef95d07732758e7ffcd9b726f
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-02-12 19:58:02 -0800

    Prop drill timestamp

--- src/client/components/RightContainer/components/QueryLog.jsx
+++ src/client/components/RightContainer/components/QueryLog.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function QueryLog({ latency }) {
+export default function QueryLog({ timestamp, latency }) {
 
   return (
     <div id="query-log">
-      <div>{new Date()}</div>
+      <div>{timestamp}</div>
       <div>Test Query</div>
       <div>{latency} ms</div>
     </div>
