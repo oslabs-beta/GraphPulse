fa3f9510bf98b73ccef38f2dfd83c30cb8d10d08
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-02-14 17:20:59 -0800

    Fix double function declaration from previous merge resolution

--- src/client/components/LeftContainer/LContComponents/QLogInput.jsx
+++ src/client/components/LeftContainer/LContComponents/QLogInput.jsx
@@ -23,9 +23,6 @@ const [adjustedOperation, setAdjustedOperation] = useState(`{
       email
   }
 }`);
-
-
-function QLogInput({ qInput, setQInput, results, setResults }) {
   // States for query and results
   const [getLazyResults, { loading, data }] = useLazyQuery(
     gql`
