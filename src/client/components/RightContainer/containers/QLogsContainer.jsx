e90fd8c833edf4e02b2dcc57ceb1f9a2800a4835
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-02-18 15:50:48 -0500

    Fetch and dynamically render user queries when users are signed in

--- src/client/components/RightContainer/containers/QLogsContainer.jsx
+++ src/client/components/RightContainer/containers/QLogsContainer.jsx
@@ -1,16 +1,48 @@
 import React, { useState, useEffect } from "react";
 
-function QLogsContainer({ latency, depth, uri}) {
+function QLogsContainer({ latency, depth, uri, isGuest}) {
   const sectionNames = ['Timestamp', 'Endpoint', 'Latency (ms)', 'Depth'];
 
   const [queryLogs, setQueryLogs] = useState([]);
   useEffect(() => {
+    if (!isGuest) {
+      // Fetch user's query logs from the server
+      fetch(`/api/querylogs`)
+        .then(response => response.json())
+        .then(data => {
+          setQueryLogs(data)
+          
+        })
+        .catch(error => console.error(error));
+    }
+  }, []);
+
+  useEffect(() => {
     if (latency > 0 && depth > 0)  {
       const today = new Date();
       const timestamp = today.toDateString();
-      setQueryLogs((queryLogs) => [...queryLogs, [timestamp, uri, latency, depth]]);
+      const newLog = [timestamp, uri, latency, depth];
+      setQueryLogs((queryLogs) => [...queryLogs, newLog]);
+
+      if (!isGuest) {
+        // Post new query log to the server
+        fetch('/api/addquerylog', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            timestamp: timestamp,
+            endpoint: uri,
+            latency: latency,
+            depth: depth,
+          }),
+        })
+        .catch(error => console.error(error));
+      }
     }
   }, [latency, depth]);
+
   return (
     <div id="qlog-container">
       <div id="qlog-info-container">
@@ -29,11 +61,20 @@ function QLogsContainer({ latency, depth, uri}) {
               // Render rows only when there are data rows
               queryLogs.map((el, i) => (
                 <tr key={i} className="qlog-table-row">
-                  {el.map((item, j) => (
-                    <td key={`${i}-${j}`} className="qlog-table-data">
-                      {item}
-                    </td>
-                  ))}
+                  {Array.isArray(el) ? (
+                    el.map((item, j) => (
+                      <td key={`${i}-${j}`} className="qlog-table-data">
+                        {item}
+                      </td>
+                    ))
+                  ) : (
+                    <>
+                      <td className="qlog-table-data">{el.timestamp}</td>
+                      <td className="qlog-table-data">{el.endpoint}</td>
+                      <td className="qlog-table-data">{el.latency}</td>
+                      <td className="qlog-table-data">{el.depth}</td>
+                    </>
+                  )}
                 </tr>
               ))
             ) : (
