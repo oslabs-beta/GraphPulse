3a19a690700b96b2d4b4f173099b4afbbdf0f420
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-03-14 17:57:29 -0400

    Update endpoint styling to show connection

--- src/client/components/LeftContainer/LContComponents/EndPointBar.jsx
+++ src/client/components/LeftContainer/LContComponents/EndPointBar.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { request } from 'graphql-request';
 
 function EndPointBar({endpoint, setEndpoint, setUri}) {
 
+  const [isSent, setIsSent] = useState(false);
+
 
   const handleEndpointSubmit = () => {
+    if(endpoint === '') return;
     setUri(endpoint);
     window.alert(`Endpoint set to: ${endpoint}!`);
+    setIsSent(true); 
     console.log(endpoint);
   };
+
+  useEffect(() => {
+    if(endpoint === '') setIsSent(false);
+},[endpoint]);
+
   return (
   <div id="endpoint-container">
     <button id="endpoint-send-btn" onClick={handleEndpointSubmit}>Send</button>
@@ -18,6 +27,12 @@ function EndPointBar({endpoint, setEndpoint, setUri}) {
       placeholder="Enter URL or endpoint" 
       value={endpoint}
       onChange={(e) => setEndpoint(e.target.value)}
+      style={{
+        backgroundColor: isSent && endpoint !== '' ? '#4a6741' : '#201d34',
+        borderColor: isSent && endpoint !== '' ? 'lightgreen' : '#b0a2b9',
+        color: isSent  && endpoint !== '' ? 'white' : '#b0a2b9',
+        transition: 'all 0.2s ease-in-out'
+      }}
       />
 
  </div>
