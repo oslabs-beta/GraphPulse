d604350fa69ac9ab072c86e257f3f5f02cff8555
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-05-23 13:49:32 -0400

    Install and configure dependencies for cypress and coverage

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+      return config
+    },
+    baseUrl: "http://localhost:8080",
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
     },
   },
 });
